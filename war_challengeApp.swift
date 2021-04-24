6f4aa6fb7996d6d2e1f007243f21b4ac42d94f2e
Chris Capper <55027395+chris-capper@users.noreply.github.com>
2021-04-24 12:04:23 -0700

    Add files via upload
    Updating UI

---
+++ war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Chris Capper on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
